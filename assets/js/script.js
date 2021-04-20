27bd854f56094e7a0b7abf47871efcb7feff44de
Adrien <adrien.payet@outlook.com>
2021-04-20 20:56:22 +0200

    Fonction filtrage par pratique ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,99 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        addToSelection: () => {
-            
+document.addEventListener('DOMContentLoaded', () => {
+
+    // =========================================== OBJECTS
+
+    const tools = {
+        cards: document.querySelectorAll('.tool'),
+        refresh: () => {
+            tools.cards.forEach((tool, index) => {
+
+                const tags = tool.dataset.tags.split(',')
+
+                if (!tags) return
+                if (delivery.activeFilters.length === 0) {
+                    tool.classList.remove('hide')
+                    return
+                }
+
+                if (tags.some(tag => delivery.activeFilters.includes(tag))) {
+                    tool.classList.remove('hide')
+                } else {
+                    tool.classList.add('hide')
+                }
+
+            })
         }
     }
+
+    const search = {
+        btn: document.querySelector('.search__btn'),
+        container: document.querySelector('.search'),
+        filters: document.querySelectorAll('.search__filter'),
+        list: document.querySelector('.search__list'),
+        activeFilters: [],
+        isOpen: false,
+        toggle: () => {
+            search.container.classList.toggle('search--open')
+            
+            search.isOpen = !search.isOpen
+        }
+    }
+
+    const selection = {
+        counter: document.querySelector('.count'),
+        count: 0,
+        list: document.querySelector('.selection__list'),
+        refresh: () => {
+            selection.count = delivery.activeFilters.length
+            selection.counter.textContent = selection.count
+        }
+    }
+
+    const delivery = {
+        activeFilters: [],
+        addToSelection: target => {
+            const tag = target.textContent
+            delivery.activeFilters.push(tag)
+
+            const li = document.createElement('li')
+            li.innerHTML = `<button class="selection__filter">${tag}</button>`
+            selection.list.appendChild(li)
+
+            target.parentNode.removeChild(target)
+        },
+        
+        removeFromSelection: target => {
+            if (!target) return
+            const tag = target.textContent
+            delivery.activeFilters = delivery.activeFilters.filter(item => item !== tag)
+
+            const li = document.createElement('li')
+            li.innerHTML = `<button class="search__filter">${tag}</button>`
+            search.list.appendChild(li)
+
+
+            target.parentNode.removeChild(target)
+        }
+    }
+
+    // =========================================== INTERACTIONS
+
+    search.btn.addEventListener('click', () => {
+        search.toggle()
+    })
+
+    search.list.addEventListener('click', event => {
+        const target = event.target.closest('.search__filter')
+        delivery.addToSelection(target)
+        selection.refresh()
+        tools.refresh()
+    })
+
+    selection.list.addEventListener('click', event => {
+        const target = event.target.closest('.selection__filter')
+        delivery.removeFromSelection(target)
+        selection.refresh()
+        tools.refresh()
+    })
+
 })
--- site/templates/home.php
+++ site/templates/home.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="<?= url('assets') ?>/css/style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> 
-    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js" defer></script>
     <script src="<?= url('assets') ?>/js/script.js" defer></script>
 
     <meta property='og:title' content='<?= $site->title() ?>' />
@@ -24,19 +23,46 @@
 </head>
 <body>
     <div id="app">
-        <header class="header">
-            <button class="header__search"></button>
-            <button class="header__selection"></button>
+        <!--========== HEADER ==========-->
+        <header class="mainHeader">
+
+            <!--========== SEARCH ==========-->
+            <div class="mainHeader__search search">
+                <header class="search__btn">
+                    <h1>Définir ma recherche</h1>
+                </header>
+                <div class="search__content">
+                    <h3>Sélectionner vos pratiques à outiller</h3>
+                    <ul class="search__list">
+                        <?php foreach($site->tagsGroup()->toStructure()->pluck('tags', ',', true) as $tag): ?>
+                            <li><button class="search__filter"><?= $tag ?></button></li>
+                        <?php endforeach ?>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="mainHeader__selection selection">
+                <header class="selection__btn">
+                    <h1>Sélection</h1>
+                </header>
+                <div class="selection__content">
+                    <h3>Filtres par pratique (<span class="count">0</span>)</h3>
+                    <ul class="selection__list">
+                        
+                    </ul>
+                </div>
+            </div>
         </header>
         
+        <!--========== TOOLS ==========-->
         <div class="tools">
         <?php foreach($site->children()->listed() as $tool): ?>
-            <section class="tool">
+            <section class="tool" data-tags="<?= $tool->tags() ?>">
                 <header class="tool__header">
-                    <h1><?= $tool->title() ?></h1>
+                    <h2><?= $tool->title() ?></h2>
                 </header>
                 <div class="tool__summary summary">
-                    <h2><?= $tool->fonction() ?></h2>
+                    <h3><?= $tool->fonction() ?></h3>
                     <p><?= $tool->summary() ?></p>
                     <ul class="summary__tags">
                         <?php foreach($tool->tags()->split() as $tag): ?>
